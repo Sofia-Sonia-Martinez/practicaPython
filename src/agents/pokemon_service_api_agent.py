70f8a17b19f8a1e7b0b60614b046d098ae72d0dd
Sofia-Sonia-Martinez <sofiasoniamar99@gmail.com>
2023-10-09 15:50:00 -0300

    add agent of service api agent

---
+++ src/agents/pokemon_service_api_agent.py
@@ -0,0 +1,8 @@
+from src.interfaces.pokemon_api_interface import *
+
+class PokemonServiceApiAgent(PokemonApiInterface):
+    
+     def find_pokemon(self, name, abiliti):
+            return [ {"name":"ditto","abiliti":"limber"} , {"name":"ditto","abiliti":"limber"} ]  
+
+
