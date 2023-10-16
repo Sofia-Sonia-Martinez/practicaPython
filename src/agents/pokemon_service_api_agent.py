8bc0dbf5cb8b9ac7465c9e4e91b74a22f6dfed87
Sofia-Sonia-Martinez <sofiasoniamar99@gmail.com>
2023-10-16 13:41:03 -0300

    refactor: modularice el agente en muchos metodos distintos

--- src/agents/pokemon_service_api_agent.py
+++ src/agents/pokemon_service_api_agent.py
@@ -1,17 +1,38 @@
 import requests
 import json
+from requests.exceptions import JSONDecodeError, ConnectionError, ConnectTimeout 
 from src.interfaces.pokemon_api_interface import *
 
 class PokemonServiceApiAgent(PokemonApiInterface):
-    
-    def find_pokemon(self, name):
-        self.name = name
-        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{self.name}').json()
-        abilities = response["abilities"]
-        ability=[]
+   
+
+    def find_pokemon(self,name):
+        ability_list = ()
+        response = self.make_request(name)
+        parsed = self.parsed_json(response)
+        abilities_list=self.search_abilities(parsed)
+        return name, abilities_list
+ 
+    def search_abilities(self,parsed):
+        abilities = parsed["abilities"]
+        ability = []
         for ability_info in abilities:
             ability_name = ability_info["ability"]["name"]
             ability.append(ability_name)
-        return name, ability
+        return ability 
 
+    def make_request(self,name):
+        try:
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+            return response
+        except (ConnectionError, ConnectTimeout):
+            return [{}]
 
+    def parsed_json(self,response = requests.models.Response):
+        resp_val = []
+        try:
+            resp_val = response.json()
+        except JSONDecodeError as e:
+            print("error al parsear la respuesta de la api a json")
+        return resp_val 
+    
